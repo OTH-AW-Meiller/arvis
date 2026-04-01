60b3a04f500e49f1fafe6ad7877e565b54cab5c2
meiller <meiller@mi-01.oth-aw.de>
2026-04-01 09:10:09 +0200

    importmap

--- scripts/three-barchart.js
+++ scripts/three-barchart.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'OrbitControls';
 
 const container = document.getElementById('container');
 const scene = new THREE.Scene();
